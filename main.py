daea5c3f66c4ef2e45d227e2b0aa569020d4efdb
Miguel Hernandez Liebano <mhliebano@gmail.com>
2014-04-28 08:18:00 -0430

    Archivo de ejecución de la aplicación que importa los paquetes acciones y ventana, creando instancias de ellos

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-#usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from acciones import *
+from ventana import *
+
+if __name__ == "__main__":
+    miventana = Ventana()
+    misacciones=Acciones(miventana)
+    miventana.hojaBienvenida()
+    miventana.main()
 
